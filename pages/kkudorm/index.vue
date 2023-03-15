945590af4bc1253e7bbef3aecca3c278835b3c21
kkpanuwat <panuwat20481@gmail.com>
2023-03-15 16:55:20 +0700

    feature:get dorm by id

---
+++ pages/kkudorm/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-container v-show="dorm_data != null">
+    {{ dorm_data }}
+  </v-container>
+</template>
+
+<script>
+export default {
+  data() {
+    return { dormID: String, drom_name: String, dorm_data: null };
+  },
+  async created() {
+    this.dorm_name = this.$route.query.dorm_name;
+    this.dormID = this.$route.query.dormID;
+    this.dorm_data = await this.$kkudormAPI.getDormByID(this.dormID);
+  },
+};
+</script>
+
+<style>
+</style>
